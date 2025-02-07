9ae830cfdb5dbf3bfb4233f4c9f7145c212f9739
Akshi-332 <akshithaha942@gmail.com>
2025-02-07 15:46:00 +0530

    Day 1 assignment

--- debit.java
+++ debit.java
@@ -2,9 +2,6 @@ public class debit {
     public static void main(String[] args) {
        
         int[] transactions = {900, -1500, 600, -1200, 400};
-
-        
-
         int debitCount = 0;
         for (int transaction : transactions) {
             if (transaction < 0) { 
