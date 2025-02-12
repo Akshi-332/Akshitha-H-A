547218dbf472c13805b494379143747c16f2b84b
Akshi-332 <akshithaha942@gmail.com>
2025-02-12 15:22:40 +0530

    practice

---
+++ Day5/FreqViaHT.java
@@ -0,0 +1,16 @@
+import java.util.Hashtable;
+
+public class FreqViaHT{
+    public static void main(String[] args) {
+        int[] transactions = {800, 1200, 4500, 320, 600, 800, 560, 4500, 12000, 320};
+        Hashtable<Integer, Integer> myTable = new Hashtable<>();
+
+        for (int index = 0; index < transactions.length; index++) {
+            int currentAmount = transactions[index];
+            int currentOccurrence = myTable.getOrDefault(currentAmount, 0);
+            myTable.put(currentAmount, currentOccurrence + 1);
+        }
+
+        System.out.println(myTable);
+    }
+}
